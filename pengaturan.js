83bf8141bbdc5dbfb7d481acbfd9fc304e99814d
jurupotostudio <jurupotostudio@gmail.com>
2026-09-09 11:06:06 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -8,6 +8,6 @@ const CONFIG_APP = {
     namaVendor: "JURUPOTO STUDIO",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
-    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
-    whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
+    googleApiKey: "AIzaSyC4I5_0e2Ov86MQPnLLFecc9VR6x9FkYpY",
+    whatsappAdmin: "6287819468729" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
